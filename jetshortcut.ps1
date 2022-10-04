7724eb54ef6581beb6dc14c57e44958748608676
Guilherme Kubagawa <57578598+guikuba@users.noreply.github.com>
2022-10-04 19:39:35 -0300

    Update jetshortcut.ps1
    add jetbrains path and include symboliclink to jetbrains app in public user desktop folder

--- jetshortcut.ps1
+++ jetshortcut.ps1
@@ -1,14 +1,14 @@
-$APPDATA = $env:APPDATA
+$APPDATA = $env:LOCALAPPDATA
 $USERS = @(Get-ChildItem C:\Users\ -Name)
 
 foreach ($USER IN $USERS) {
-  if (-not ($USER -eq 'Public')) {
-    $UPATCH = "C:\Users\$USER\Desktop\JET"
-    $APPDATAJET = "$APPDATA\Telegram Desktop\Telegram.exe"
+  if ($USER -eq 'Public') {
+    $UPATCH = "C:\Users\$USER\Desktop\JETBRAINS TOOLBOX"
+    write-host $USER
+    $APPDATAJET = "$APPDATA\JetBrains\Toolbox\bin\jetbrains-toolbox.exe"
     New-Item -ItemType SymbolicLink -Path $UPATCH -Target $APPDATAJET
   }
   else {
     
   }
 }
-
